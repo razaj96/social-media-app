4f029e38fea77365a7d20bfb3f4f4699e9ed2936
Raza Jaffery <110700927+razaj96@users.noreply.github.com>
2022-12-26 11:16:23 -0800

    added followerscard component

--- src/components/profileside/ProfileSide.jsx
+++ src/components/profileside/ProfileSide.jsx
@@ -1,12 +1,14 @@
 import LogoSearch from "../LogoSearch/LogoSearch";
 import ProfileCard from "../ProfileCard/ProfileCard";
 import "./ProfileSide.css";
+import FollowersCard from "../FollowersCard/FollowersCard";
 
 const ProfileSide = () => {
   return (
     <div className="ProfileSide">
       <LogoSearch></LogoSearch>
       <ProfileCard></ProfileCard>
+      <FollowersCard></FollowersCard>
     </div>
   );
 };
