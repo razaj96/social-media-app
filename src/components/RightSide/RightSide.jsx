421b4b14607aa376036e0e3317535c4f65c384cc
Raza Jaffery <110700927+razaj96@users.noreply.github.com>
2022-12-26 18:34:46 -0800

    added the top navbar and styled it

--- src/components/RightSide/RightSide.jsx
+++ src/components/RightSide/RightSide.jsx
@@ -1,7 +1,21 @@
 import "./RightSide.css";
+import Home from "../../img/home.png";
+import Noti from "../../img/noti.png";
+import Comment from "../../img/comment.png";
+import { UilSetting } from "@iconscout/react-unicons";
 
 const RightSide = () => {
-  return <div className="RightSide">RightSide</div>;
+  return (
+    <div className="RightSide">
+      <div className="navIcons">
+        <img src={Home} alt="" />
+        <UilSetting></UilSetting>
+        <img src={Noti} alt="" />
+        <img src={Comment} alt="" />
+      </div>
+      <TrendCard></TrendCard>
+    </div>
+  );
 };
 
 export default RightSide;
