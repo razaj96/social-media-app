d7cc32c8b88112e461d54b9558d063f9e7dc1fb3
Raza Jaffery <110700927+razaj96@users.noreply.github.com>
2022-12-26 18:21:55 -0800

    added RightSide Component

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,13 +1,14 @@
 import "./Home.css";
 import ProfileSide from "../../components/profileside/ProfileSide";
 import PostSide from "../../components/PostSide/PostSide";
+import RightSide from "../../components/RightSide/RightSide";
 
 const Home = () => {
   return (
     <div className="Home">
       <ProfileSide></ProfileSide>
       <PostSide></PostSide>
-      <div className="rightSide">Right</div>
+      <RightSide></RightSide>
     </div>
   );
 };
